057facf1431bb4906ae90080b6bd0c32e960c77d
kwonhygge <kitteniso1103@gmail.com>
2020-09-19 20:11:10 +0900

    Fix the problem that toyproject's font and work's font are different

--- src/components/pages/ToyProject/ToyInfo/ToyInfo.js
+++ src/components/pages/ToyProject/ToyInfo/ToyInfo.js
@@ -7,31 +7,33 @@ const ToyInfo = (props) => {
   const item = props.item;
   return (
     <>
-      <div className={styles.Header}>
-        <h1>{item.index}</h1>
-        <h2>{item.name}</h2>
-      </div>
-      <div className={bookStyles.ContentContainer}>
-        <div className={styles.Content}>
-          <div>
-            <h1>인원</h1>
-            <span>{item.member} 프로젝트</span>
-          </div>
-          <div>
-            <h1>기간</h1>
-            <span>{item.period}</span>
-          </div>
-          <div>
-            <h1>목표</h1>
-            <span>{item.objective}</span>
-            <span style={{ fontWeight: '700' }}>{item.boldObj}</span>
-          </div>
-          <div>
-            <h1>사용 프로그램</h1>
-            <span>{item.program}</span>
-          </div>
+      <div className={styles.Container}>
+        <div className={styles.Header}>
+          <h1>{item.index}</h1>
+          <h2>{item.name}</h2>
         </div>
-        <Colors colorlist={item.colors} />
+        <div className={bookStyles.ContentContainer}>
+          <div className={styles.Content}>
+            <div>
+              <h1>인원</h1>
+              <span>{item.member} 프로젝트</span>
+            </div>
+            <div>
+              <h1>기간</h1>
+              <span>{item.period}</span>
+            </div>
+            <div>
+              <h1>목표</h1>
+              <span>{item.objective}</span>
+              <span style={{ fontWeight: '700' }}>{item.boldObj}</span>
+            </div>
+            <div>
+              <h1>사용 프로그램</h1>
+              <span>{item.program}</span>
+            </div>
+          </div>
+          <Colors colorlist={item.colors} />
+        </div>
       </div>
     </>
   );
