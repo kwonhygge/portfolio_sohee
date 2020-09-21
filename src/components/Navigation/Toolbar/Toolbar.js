e944af425e153e7155e041717beaf27c866bb64c
kwonhygge <kitteniso1103@gmail.com>
2020-09-21 14:51:01 +0900

    Home logo changed

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -63,7 +63,7 @@ class Toolbar extends Component {
         <header className={styles.Toolbar}>
           <div className={styles.ToolbarContent}>
             <div>
-              <Link to="/" exact="true">
+              <Link to="/" exact="true" className={styles.HomeLogo}>
                 <HomeLogo />
               </Link>
               <div
