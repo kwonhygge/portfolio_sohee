49b7f213e11b70f97d1fd08f51f40071a243f4ce
kwonhygge <kitteniso1103@gmail.com>
2020-09-23 00:08:41 +0900

    Updates

--- precache-manifest.c1c5e909fd66682e3502b68c2ec9c52f.js
+++ precache-manifest.c1c5e909fd66682e3502b68c2ec9c52f.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1ccb6d4e08e20da5f14d2e42ad00c52a",
+    "revision": "a49cfa279203ea4a86aa92b7cc977fad",
     "url": "/portfolio_sohee/index.html"
   },
   {
-    "revision": "8a4d4ca63a4ea0923463",
-    "url": "/portfolio_sohee/static/css/main.8865351f.chunk.css"
+    "revision": "2bd57d6b28c876dfba38",
+    "url": "/portfolio_sohee/static/css/main.66f1e48f.chunk.css"
   },
   {
     "revision": "b5353418baea7bc8e81d",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio_sohee/static/js/2.57c398f3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8a4d4ca63a4ea0923463",
-    "url": "/portfolio_sohee/static/js/main.9e0f29cb.chunk.js"
+    "revision": "2bd57d6b28c876dfba38",
+    "url": "/portfolio_sohee/static/js/main.07401079.chunk.js"
   },
   {
     "revision": "6ae957c1dbfe63a905cf",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio_sohee/precache-manifest.8baef7863cac9f3f1d90d0bcae83864d.js"
+  "/portfolio_sohee/precache-manifest.c1c5e909fd66682e3502b68c2ec9c52f.js"
 );
 
 self.addEventListener('message', (event) => {
