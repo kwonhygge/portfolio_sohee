b8e75b1301aacc5814b491f3b760b6467cb78dd9
kwonhygge <kitteniso1103@gmail.com>
2020-09-24 17:26:45 +0900

    Drawing page's images completed

---
+++ src/assets/images/drawing/index.js
@@ -0,0 +1,15 @@
+import Draw1 from './1.jpg';
+import Draw2 from './2.jpg';
+import Draw3 from './3.jpg';
+import Draw4 from './4.jpg';
+import Draw5 from './5.png';
+import Draw6 from './6.jpg';
+
+export default [
+  { src: Draw1, alt: 'Draw1' },
+  { src: Draw2, alt: 'Draw2' },
+  { src: Draw3, alt: 'Draw3' },
+  { src: Draw4, alt: 'Draw4' },
+  { src: Draw5, alt: 'Draw5' },
+  { src: Draw6, alt: 'Draw6' },
+];
--- src/components/pages/ToyProject/Drawing/Drawing.js
+++ src/components/pages/ToyProject/Drawing/Drawing.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Drawing.module.css';
+import drawingList from '../../../../assets/images/drawing';
 
 const Drawing = () => (
   <>
@@ -7,10 +8,23 @@ const Drawing = () => (
       <h1>Drawing</h1>
       <div>
         <span>사용 프로그램</span>
-        <span>illustration, clipstudio</span>
+        <span>illustrator, clipstudio</span>
       </div>
     </div>
-    <div className={styles.Gallery}></div>
+    <div className={styles.Gallery}>
+      <div className={styles.Row}>
+        <div className={styles.Col}>
+          <img src={drawingList[0].src} />
+          <img src={drawingList[1].src} />
+          <img src={drawingList[2].src} />
+        </div>
+        <div className={styles.Col}>
+          <img src={drawingList[3].src} />
+          <img src={drawingList[4].src} />
+          <img src={drawingList[5].src} />
+        </div>
+      </div>
+    </div>
   </>
 );
 
