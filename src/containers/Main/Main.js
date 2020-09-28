d71130c8744fafbe9f7c4e5f9bdda025572dd350
kwonhygge <kitteniso1103@gmail.com>
2020-09-28 23:29:53 +0900

    Add animation for i and dot

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styles from './Main.module.css';
+import stylesHome from '../../components/pages/Home/Home.module.css';
 
 class Main extends Component {
   constructor(props) {
@@ -40,20 +41,20 @@ class Main extends Component {
           </div>
         )}
         {!loading && (
-          <div className={styles.MainContainer}>
-            <div className={styles.Upper}>
+          <div className={stylesHome.MainContainer}>
+            <div className={stylesHome.Upper}>
               <span>P</span>
               <span>O</span>
               <span>R</span>
               <span>T</span>
             </div>
-            <div className={styles.Down}>
+            <div className={stylesHome.Down}>
               <span>F</span>
               <span>O</span>
               <span>L</span>
-              <span className={styles.I}>I</span>
-              <span className={styles.O}>O</span>
-              <span className={styles.Dot}>.</span>
+              <span className={stylesHome.I}>I</span>
+              <span className={stylesHome.O}>O</span>
+              <span className={stylesHome.Dot}></span>
             </div>
           </div>
         )}
